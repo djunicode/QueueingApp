44daa211dc33bb04e4240ebfed5ebcde96c51a0c
Ruturaj123 <ruturaj2305@gmail.com>
2018-02-04 08:56:26 +0530

    Email Verification

--- app/src/main/java/com/djunicode/queuingapp/activity/MainActivity.java
+++ app/src/main/java/com/djunicode/queuingapp/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.djunicode.queuingapp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import com.djunicode.queuingapp.R;
 
 /**
@@ -10,12 +11,18 @@ import com.djunicode.queuingapp.R;
  */
 public class MainActivity extends AppCompatActivity {
 
+  private CardView signUpStudentCardView, logInStudentCardView, signUpTeacherCardView, logInTeacherCardView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    /*Intent intent = new Intent(this, LogInActivity.class);
-    startActivity(intent);*/
+    signUpStudentCardView = (CardView) findViewById(R.id.signUpStudentCardView);
+    logInStudentCardView = (CardView) findViewById(R.id.logInStudentCardView);
+    signUpTeacherCardView = (CardView) findViewById(R.id.signUpTeacherCardView);
+    logInTeacherCardView = (CardView) findViewById(R.id.logInTeacherCardView);
+
+
   }
 }
